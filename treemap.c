21e69e51aa2e0a893ebd4577f1de8161b1ca516f
Your Name <you@example.com>
2022-04-21 11:29:42 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -75,12 +75,12 @@ void eraseTreeMap(TreeMap * tree, void* key){
 Pair * searchTreeMap(TreeMap * tree, void* key) {
     TreeNode* nodo = tree->root;
     while(nodo != NULL){
-        if(nodo->pair->key == key){
+        if(nodo->pair->key > key) nodo = nodo->right;
+        else if(nodo->pair->key < key) nodo = nodo->left;
+        else{
             tree->current = nodo;
             return nodo->pair;
         }
-        if(nodo->pair->key > key) nodo = nodo->right;
-        if(nodo->pair->key < key) nodo = nodo->left;
     }
     return NULL;
 }
