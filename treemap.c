35869cec5a63c2b07e8e040d7b870b3e887a94c5
Your Name <you@example.com>
2022-04-21 15:18:54 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -194,7 +194,10 @@ Pair * nextTreeMap(TreeMap * tree) {
     if(nodo->right == NULL){
         while(nodo->parent != NULL){
             if(tree->lower_than(nodo->parent->pair->key,tree->current->pair->key)==1) nodo = nodo->parent;
-            else if(tree->lower_than(tree->current->pair->key,nodo->parent->pair->key)==1) return nodo->parent->pair;
+            else if(tree->lower_than(tree->current->pair->key,nodo->parent->pair->key)==1){
+                tree->current = nodo->parent;
+                return tree->current->pair;
+            }
         }
     }
 
