64bde691755966176d172f901051846d03bae748
Your Name <you@example.com>
2022-04-22 23:39:06 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -178,12 +178,13 @@ Pair * upperBound(TreeMap * tree, void* key) {
 
     while (nodo != NULL){
         if(tree->lower_than(nodo->pair->key,key)==1){
+            if(tree->lower_than(nodo->pair->key,UB_node->pair->key)==1) UB_node = nodo;
             nodo = nodo->right;
         }
 
         else if(tree->lower_than(key,nodo->pair->key)==1){
+            if(tree->lower_than(nodo->pair->key,UB_node->pair->key)==1) UB_node = nodo;
             nodo = nodo->left;
-            UB_node = nodo;
 
         } 
         
