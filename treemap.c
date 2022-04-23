eed4bfd49f709d693215a035f5247dd38c2ba206
Your Name <you@example.com>
2022-04-23 15:38:46 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -175,17 +175,20 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 Pair * upperBound(TreeMap * tree, void* key) {
     TreeNode* nodo = tree->root;
     TreeNode* UB_node= nodo;
+    int cont=0;
 
     while (nodo != NULL){
         if(tree->lower_than(nodo->pair->key,key)==1){
-            if(tree->lower_than(UB_node->pair->key,nodo->pair->key)==1){
+            cont++;
+            if(tree->lower_than(UB_node->pair->key,nodo->pair->key)==1 && cont != 0){
                 UB_node = nodo;
             }
             nodo = nodo->right;
         }
 
         else if(tree->lower_than(key,nodo->pair->key)==1){
-            if(tree->lower_than(UB_node->pair->key,nodo->pair->key)==1){
+            cont++;
+            if(tree->lower_than(UB_node->pair->key,nodo->pair->key)==1 && cont != 0){
                 UB_node = nodo;
             }
             nodo = nodo->left;
