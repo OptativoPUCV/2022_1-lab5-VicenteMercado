671b7863b4d097aa56770217cf54d2411be03aa4
Your Name <you@example.com>
2022-04-23 20:21:53 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -198,7 +198,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
             return nodo->pair;
         }
 
-        printf("Key: %d \n Nodo: %d \n UB:%d\n\n", * (int *) key, * (int *) nodo->pair->key, * (int *) UB_node->pair->key);
+        //printf("Key: %d \n Nodo: %d \n UB:%d\n\n", * (int *) key, * (int *) nodo->pair->key, * (int *) UB_node->pair->key);
     }
 
     return UB_node->pair;
